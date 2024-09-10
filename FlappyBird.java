60c9fb2784e38ea37899c10eb208193fadeb1fd3
smarttadbeer <mubashir@globalgov.ae>
2024-09-10 07:04:27 +0530

    image into file

--- FlappyBird.java
+++ FlappyBird.java
@@ -8,6 +8,12 @@ public class FlappyBird extends JPanel {
     int bWidth = 360;
     int bHeight = 640;
 
+    //images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
     FlappyBird() {
         setPreferredSize(new Dimension(bWidth, bHeight));
         setBackground(Color.blue);
