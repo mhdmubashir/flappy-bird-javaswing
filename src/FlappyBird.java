a747c1965220f31d68b17c175b6d570f9a8e4565
smarttadbeer <mubashir@globalgov.ae>
2024-09-10 07:26:09 +0530

    some changes like bird deatils and etc

---
+++ src/FlappyBird.java
@@ -0,0 +1,59 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel {
+    int bWidth = 360;
+    int bHeight = 640;
+
+    //images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+    //bird
+    int birdX = bWidth/8;
+    int birdY = bHeight/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+    
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+ 
+
+
+
+    FlappyBird() {
+        setPreferredSize(new Dimension(bWidth, bHeight));
+        //setBackground(Color.blue);
+        
+        //loadimage
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImgImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+        
+    }
+
+    public void paintComponent (Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        //bg
+        g.drawImage(backgroundImg, 0, 0, bWidth, bHeight)
+    }
+}
