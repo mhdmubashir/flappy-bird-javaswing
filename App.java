592feeecaf7d92b1bf90d344d42bc731b74b533a
smarttadbeer <mubashir@globalgov.ae>
2024-09-10 06:54:36 +0530

    Frame of game

---
+++ App.java
@@ -0,0 +1,14 @@
+import java.swing.*;
+public class App {
+    public static void main(String args[]) {
+        int bWidth = 360;
+        int bHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(bWidth, bHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
